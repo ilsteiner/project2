fb5c0ca39d9ae9ed4a2286284f099ce009c96c43
Isaac <izakizak@gmail.com>
2016-09-23 13:43:47 -0400

    Removed code to show errors

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-ini_set('display_errors', 1); # Display errors on page (instead of a log file)
-?>
 <!doctype html>
 <html class="no-js" lang="">
     <head>
